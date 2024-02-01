a6f464dc463492ac382faa17e4f7342b2fff11c8
PiotrPress <admin@piotr.press>
2024-02-01 17:48:25 +0100

    v0.2.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Meta Block
  * Plugin URI: https://github.com/PiotrPress/wordpress-meta-block
  * Description: This WordPress plugin adds a Meta Block which renders a selected post meta field.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Requires at least: 6.4
  * Requires PHP: 7.4
  * Author: Piotr Niewiadomski
@@ -18,14 +18,12 @@
 
 defined( 'ABSPATH' ) or exit;
 
-add_action( 'init', function() {
-    register_block_type( __DIR__, [
-        'render_callback' => fn( $attributes ) => apply_filters( 'piotrpress/meta_block', get_post_meta( get_the_ID(), $attributes[ 'key' ], true ), $attributes[ 'key' ], get_the_ID() ),
-        'attributes' => [
-            'key' => [
-                'type' => 'string',
-                'default' => ''
-            ]
+add_action( 'init', function() { register_block_type( __DIR__, [
+    'render_callback' => fn( $attributes ) => $attributes[ 'key' ] ? apply_filters( 'piotrpress/meta_block', get_post_meta( get_the_ID(), $attributes[ 'key' ], true ), $attributes[ 'key' ], get_the_ID() ) : null,
+    'attributes' => [
+        'key' => [
+            'type' => 'string',
+            'default' => ''
         ]
-    ] );
-} );
+    ]
+] ); } );
